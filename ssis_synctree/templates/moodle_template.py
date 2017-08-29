b53e4c1e8d3d89c6b125a3c4a35b9474da1e307f
Adam Morris <amorris@mistermorris.com>
2017-08-29 22:03:40 +0800

    turn off unenrollments

--- ssis_synctree/templates/moodle_template.py
+++ ssis_synctree/templates/moodle_template.py
@@ -423,21 +423,23 @@ class MoodleFullTemplate(MoodleFirstRunTemplate):
         # return self.php.unenrol_user_from_course(user_idnumber, course)
 
         """
-        if action.idnumber[-1] in ['0', '1']:
-            # If it's a staff member, don't do anything
-            return dropped_action(method="Not unenrolling staff {} from {}".format(action.idnumber, action.value))
+        # TODO: Make this a setting?
+        return dropped_action(method="Unenrolling is completely off")
 
-        # If it's a student or parent, just unenroll them from the group for the moment
-        if len(action.dest.courses) != len(action.dest.groups):
-            return dropped_action(method="Not removing {} from group in course {} b/c number of courses and groups unequal".format(action.idnumber, action.value))
-        group_index = action.dest.courses.index(action.value)
-        group_name = action.dest.groups[group_index]
-        if not group_name:
-            return dropped_action(method="Not removing {} from group in course {} b/c blank group found".format(action.idnumber, action.value))
+        # Previous build:
+        # if action.idnumber[-1] in ['0', '1']:
+        #     # If it's a staff member, don't do anything
+        #     return dropped_action(method="Not unenrolling staff {} from {}".format(action.idnumber, action.value))
 
-        return self.php.remove_user_from_group(action.idnumber, group_name)
+        # # If it's a student or parent, just unenroll them from the group for the moment
+        # if len(action.dest.courses) != len(action.dest.groups):
+        #     return dropped_action(method="Not removing {} from group in course {} b/c number of courses and groups unequal".format(action.idnumber, action.value))
+        # group_index = action.dest.courses.index(action.value)
+        # group_name = action.dest.groups[group_index]
+        # if not group_name:
+        #     return dropped_action(method="Not removing {} from group in course {} b/c blank group found".format(action.idnumber, action.value))
 
-        # This is really removing TODO: Make this a setting
+        # return self.php.remove_user_from_group(action.idnumber, group_name)
 
     def add_enrollments_groups_to_moodle(self, action):
         return []
