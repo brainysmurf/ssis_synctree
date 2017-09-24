ca847479d8d49f0111611ea0d729c97821bf1dd6
Adam Morris <amorris@mistermorris.com>
2017-09-24 10:00:22 +0800

    elem moved to AD

--- ssis_synctree/model/autosend_model.py
+++ ssis_synctree/model/autosend_model.py
@@ -22,20 +22,18 @@ class AutosendStudents(BaseStudents):
         # ldap_auth = ssis_synctree_settings.get('SSIS_AUTOSEND', 'auth_above_equal')
         # manual_auth = ssis_synctree_settings.get('SSIS_AUTOSEND', 'auth_less_than')
         # return ldap_auth if int(self._grade) >= boundary else manual_auth
-        if self._grade in ['6', '7', '8', '9', '10', '11', '12']:
-            return 'ldap_syncplus'
-        return 'manual'
+        return 'ldap_syncplus'
 
     @property
     def username(self):
         """
         Username is the PSID
         """
-        #return self.idnumber
-        if self._grade in ['6', '7', '8', '9', '10', '11', '12']:
-            return self.idnumber
-        mapping = ssis_synctree_settings[STUDENT_PSIDUSERNAME_MAPPINGS].get(self.idnumber)
-        return (self.name + self._year_of_graduation).lower().replace(' ', '').replace('-', '') if not mapping else mapping
+        return self.idnumber
+        # if self._grade in ['6', '7', '8', '9', '10', '11', '12']:
+        #     return self.idnumber
+        # mapping = ssis_synctree_settings[STUDENT_PSIDUSERNAME_MAPPINGS].get(self.idnumber)
+        # return (self.name + self._year_of_graduation).lower().replace(' ', '').replace('-', '') if not mapping else mapping
 
     @property
     def email(self):
