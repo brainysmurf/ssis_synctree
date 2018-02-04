005b1f5d65b202f1b28822bec4a9313b9507bd81
Adam Morris <amorris@mistermorris.com>
2018-02-04 19:20:05 +0800

    tabs to spaces

--- ssis_synctree/inform/communicate.py
+++ ssis_synctree/inform/communicate.py
@@ -1,16 +1,16 @@
 class Communicate:
 
-	def __init__(self):
+    def __init__(self):
 
-	    import ssis_synctree_settings
-    	import configparser
+        import ssis_synctree_settings
+        import configparser
 
-    	self.host = ssis_synctree_settings.get("COMMUNICATE", 'smtp')
+        self.host = ssis_synctree_settings.get("COMMUNICATE", 'smtp')
 
-    	self.sender = ssis_synctree_settings.get('COMMUNICATE', 'receivers')
-	    self.receivers = ssis_synctree_settings.get('COMMUNICATE', 'receivers').split(',')
+        self.sender = ssis_synctree_settings.get('COMMUNICATE', 'receivers')
+        self.receivers = ssis_synctree_settings.get('COMMUNICATE', 'receivers').split(',')
 
-	    self.message = """
+        self.message = """
 From: <""" + sender + """>
 To: <""" + "><".join(recievers) + """>
 MIME-Version: 1.0
@@ -18,19 +18,19 @@ Content-type: text/html
 Subject: Test
 
 """
-		
-	def compose(self, html):
-		self.message += html
+        
+    def compose(self, html):
+        self.message += html
 
-	def send(self):
-		server = smtplib.SMTP(self.host)
-		result = server.sendmail(self.sender, self.receivers, self.message)
-		if result is not {}:
-			print(result)
+    def send(self):
+        server = smtplib.SMTP(self.host)
+        result = server.sendmail(self.sender, self.receivers, self.message)
+        if result is not {}:
+            print(result)
 
 
 if __name__ == "__main__":
 
-	com = Communicate()
-	com.compose('hi')
-	com.send()
+    com = Communicate()
+    com.compose('hi')
+    com.send()
